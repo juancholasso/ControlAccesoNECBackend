ad36b5cb0cef39beb25f7583c8446ee653d6a11c
Daniel <drodriguez@nec.com.co>
2019-11-05 09:05:28 -0500

    validaciones

--- app/Http/Controllers/SubsitioController.php
+++ app/Http/Controllers/SubsitioController.php
@@ -17,7 +17,7 @@ class SubsitioController extends Controller
         $result = Subsitio::with('sitio')
 	->where('eliminado', 0)->get();
                 
-        if (count($result) > 0) {
+        if (!empty($result)) {
             return response() -> json(
                 array('data' => $result, 'message' => config('constants.messages.3.message')),
                 config('constants.messages.3.code')
